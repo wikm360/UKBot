350aac886e807a90a983d602752b06ccbb27fd92
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-03-02 21:49:56 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,35 @@ async def admin_handler (update : Update , context : CallbackContext) :
     admin_chat = 877591460
     chat_id = update.message.chat_id
     if chat_id == admin_chat :
+        await context.bot.send_chat_action(chat_id , ChatAction.TYPING)
         await context.bot.sendMessage(chat_id , "سلام گلم . به محیط ادمین خوش آمدی")
+        buttons = [
+            [
+                InlineKeyboardButton("ارسال بکاپ" , callback_data="send_backup")
+            ]
+        ]
+        await update.message.reply_text(
+            text="منو ادمین" ,
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
 
+async def query_handler(update : Update , context : CallbackContext) :
+    query = update.callback_query
+    data = query.data
+    chat_id = query.message.chat_id
+    if data == "send_backup" :
+        await send_backup_costum(update , context)
+
+async def send_backup_costum(update : Update , context : CallbackContext) :
+    admin_chat = 877591460
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    if chat_id == admin_chat :
+        with open ("./id.txt" , "rb") as file :
+            await context.bot.send_chat_action(chat_id , ChatAction.UPLOAD_DOCUMENT)
+            await context.bot.send_document(chat_id , file , caption="BackUp File" , connect_timeout = 5000)
+        #await admin_handler(update , context)
+    
 
 async def start (update : Update , context : CallbackContext) :
     chat_id = update.message.chat_id
@@ -29,6 +56,7 @@ async def start (update : Update , context : CallbackContext) :
     
     with open("./id.txt" , "a" ) as file :
         file.write(str(chat_id)+"\n")
+    await context.bot.send_chat_action(chat_id , ChatAction.TYPING)
     await context.bot.sendMessage(chat_id , " سلام " + str(fisrname) + " " + str(lastname))
     await context.bot.sendMessage(chat_id , "به ربات اضافه شدید")
 
@@ -49,6 +77,7 @@ def main () :
     application.add_handler(CommandHandler("start" , start))
     application.add_handler(CommandHandler("admin" , admin_handler))
 
+    application.add_handler(CallbackQueryHandler(query_handler))
 
     application.run_polling()
 
